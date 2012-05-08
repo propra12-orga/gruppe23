c98273f8cf47c720e31f943651cb9ee203e5cd81
Tobias Korfmacher <Tobias.Korfmacher@hhu.de>
2012-05-08 16:04:40 +0200

    Tabelle

--- Spielfeld.java
+++ Spielfeld.java
@@ -1,38 +1,18 @@
-import javax.swing.*;
+public class Spielfeld{
 
-public class Spielfeld extends JFrame {
-	
-	
-	public Spielfeld(){
-		
-		super("BomberHulk 1.0");
+	public static void main(String[] args) {
+		 	
+
 	int n = 16;//Spielfeldgröße
 	Feld[][] Spielfeld = new Feld[n][n];
-
-			/* Zufallsbelegung möglich? oder nur feste maps ;  StdDraw.picture(rx, ry, "");
-			
-              setSize(300,300);
-              setLocation(300,300);
-              setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-              setVisible(true);*/
+    	StdDraw.setXscale(0.0, 17.0);
+		StdDraw.setYscale(0.0,17.0);
 	
-         StdDraw.setXscale(0.0, 16.0);
-         StdDraw.setYscale(0.0,16.0);
-       for(int i = 0; i <n;i++){
-    	   for(int j = 0; j <n;j++){
-    		  // Spielfeld[i][j]= dra
-    		   
-    				   StdDraw.Square(8.0,8.0,8.0);
+		for(double i = 1.0; i <n;i++){
+    	   for(double j = 1.0; j <n;j++){
+    		   StdDraw.square(i,j,1.5);
+
     	   }
-       }
-              
-              
-
+		}
 	}
-	public static void main(String[] args) {
-		 
-        Spielfeld g = new Spielfeld();
-
-
-}
 }
