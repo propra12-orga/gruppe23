c0f42eb9bb303fb141243a51bf837b7652a27186
Tobias Korfmacher <Tobias.Korfmacher@hhu.de>
2012-05-11 15:44:45 +0200

    Bilder ins Spielfeld

--- src/Spielfeld.java
+++ src/Spielfeld.java
@@ -1,18 +1,47 @@
-public class Spielfeld{
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 
-	public static void main(String[] args) {
-		 	
+public class Spielfeld extends JFrame{
+	
+//	ImageIcon Hulk = new ImageIcon("Hulk.jpg");
+//  Image Hu1 = Hulk.getImage();
+//  ImageIcon Block = new ImageIcon("Block.jpg");
+//  Image Bl1 = Block.getImage();
+//  ImageIcon Mauer = new ImageIcon("Mauer.jpg");
+//  Image Ma1 = Mauer.getImage();
+//  ImageIcon Weg = new ImageIcon("Weg.jpg");
+//  Image Weg1 = Weg.getImage();	 	
 
-	int n = 16;//Spielfeldgröße
-	Feld[][] Spielfeld = new Feld[n][n];
-//    	StdDraw.setXscale(0.0, 17.0);
-//		StdDraw.setYscale(0.0,17.0);
-//	
-		for(double i = 1.0; i <n;i++){
-    	   for(double j = 1.0; j <n;j++){
-//    		   StdDraw.square(i,j,1.5);
+	
+	public void malen(int i){
+		
+		switch(i){
+			case	1: //male Hulk
+				
+			case 	2: //male Weg
+				
+			case	3: // male Mauer
+				
+			case	4: //male Block
+				
+		}
+}
+	public Spielfeld(){
 
-    	   }// nicht die draw funktion benutzen, sondern für ein n x n array jeweils viele labels erstellen mit images einfügen
+	    int n = 16;//Spielfeldgröße
+		JLabel[][] Spiel = new JLabel[n][n];
+
+		for(int i = 1; i <n;i++){
+    	   for(int j = 1; j <n;j++){
+    		   	// nicht die draw funktion benutzen, sondern für ein n x n array jeweils viele labels erstellen mit images einfügen
+    	   }
 		}
 	}
-}
+		public static void main(String args[]){
+	
+				Spielfeld first = new Spielfeld();
+			
+		}
+		
+		
+	}
