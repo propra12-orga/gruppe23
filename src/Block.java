1cc9543974713d777472473cb80a9853dabd8b15
unknown <Nobody7@Nobody_7.(none)>
2012-05-15 15:50:46 +0200

    Kommentar zur Block-Klasse hinzugefuegt

--- src/Block.java
+++ src/Block.java
@@ -1,7 +1,9 @@
-
-public class Block extends Feld{
-
-	public Block(){
-	this.wert = 2;
+public class Block extends Feld {
+	/*
+	 * statischer Block, wichtig für Kollisionsabfrage this.wert = 2 => kein
+	 * durchkommen
+	 */
+	public Block() {
+		this.wert = 2;
 	}
 }
