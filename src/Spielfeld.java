9f76385f8329372171a300abac34c49e1445abed
Tobias Korfmacher <Tobias.Korfmacher@hhu.de>
2012-05-15 13:32:57 +0200

    modified

--- src/Spielfeld.java
+++ src/Spielfeld.java
@@ -1,22 +1,25 @@
+import java.awt.Container;
+import java.awt.Graphics;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.image.ImageObserver;
+import java.awt.image.ImageProducer;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class Spielfeld extends JFrame{
-	
-//	ImageIcon Hulk = new ImageIcon("Hulk.jpg");
-//  Image Hu1 = Hulk.getImage();
-//  ImageIcon Block = new ImageIcon("Block.jpg");
-//  Image Bl1 = Block.getImage();
-//  ImageIcon Mauer = new ImageIcon("Mauer.jpg");
-//  Image Ma1 = Mauer.getImage();
-//  ImageIcon Weg = new ImageIcon("Weg.jpg");
-//  Image Weg1 = Weg.getImage();	 	
+	 	
 
-
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -8277081151052108904L;
+//	private static final long serialVersionUID = -8277081151052108904L;
 	public void malen(int i){
 		
 		switch(i){
@@ -30,44 +33,41 @@ public class Spielfeld extends JFrame{
 				
 		}
 }
-	public Spielfeld(){
+	public Spielfeld(){ 
+		final Image imageHulk  = Toolkit.getDefaultToolkit().getImage("Hulk.jpg");
+		final Image imageBlock = Toolkit.getDefaultToolkit().getImage("Block.jpg");
+		final Image imageMauer = Toolkit.getDefaultToolkit().getImage("Mauer.jpg");
+		final Image imageWeg   = Toolkit.getDefaultToolkit().getImage("Weg.jpg");
 
-	    int n = 16;//Spielfeldgr��e
+	    int n = 16;//Spielfeldgdgroesse
 		JLabel[][] Spiel = new JLabel[n][n];
+		Container cp = this.getContentPane();
 
-		for(int i = 1; i <n;i++){
-    	   for(int j = 1; j <n;j++){
-    		   	// nicht die draw funktion benutzen, sondern f�r ein n x n array jeweils viele labels erstellen mit images einf�gen
-    	   }
-		}
+		final JPanel panel = new JPanel() {
+
+			private static final long serialVersionUID = 1L;
+
+			public void paintComponent(Graphics g) {
+
+				super.paintComponent(g);
+				if (true)
+					g.drawLine(0, 0, 200, 100);
+
+				// Bild zeichnen
+				g.drawImage(imageHulk, 200, 100, 150, 100, this);
+
+			}
+		};
+
+//		for(int i = 1; i <n;i++){
+//    	   for(int j = 1; j <n;j++){
+////    		   male(1);
+//    	   }
+//		}
 	}
-		public static void main(String args[]){
 	
-				Spielfeld first = new Spielfeld();
-		
+}
 
-
-	int n = 16;//Spielfeldgr��e
-	Feld[][] Spielfelder = new Feld[n][n];
-//    	StdDraw.setXscale(0.0, 17.0);
-//		StdDraw.setYscale(0.0,17.0);
-
-//	
-		for(double i = 1.0; i <n;i++){
-    	   for(double j = 1.0; j <n;j++){
-    		   StdDraw.square(i,j,1.5);
-
-
-    	   }// nicht die draw funktion benutzen, 
-    	   //sondern f�r ein n x n array jeweils 
-    	   //viele labels erstellen mit images einf�gen
-    	   
-
-    	   }// nicht die draw funktion benutzen, sondern f�r ein n x n array jeweils viele labels erstellen mit images einf�gen
-
-    	   }// nicht die draw funktion benutzen, sondern f�r ein n x n array jeweils viele labels erstellen mit images einf�gen
-
-		}
 		
 		
 	
