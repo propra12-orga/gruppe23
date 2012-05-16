0fd24af236802101e228ca65ac8ffd0bdfdd6bf7
Sebastian Dittmann <sebastian.dittmann@uni-duesseldorf.de>
2012-05-16 09:22:51 +0200

    GameKeyListener kommentiert

--- src/GameKeyListener.java
+++ src/GameKeyListener.java
@@ -9,24 +9,27 @@ public class GameKeyListener {
 
 	
 	public void keyPressed(KeyEvent Key){ //Diese Funktion ist essentiel für die Umsetzung des Keylisteners (bzw. die einzig relevante)
-		if(Key.getKeyCode() == Key.VK_UP){
+		if(Key.getKeyCode() == Key.VK_UP){			//Oben
 			a[0] = 0;
 			a[1] = 1;
 		}
 		
-		if(Key.getKeyCode() == Key.VK_LEFT){
+		if(Key.getKeyCode() == Key.VK_LEFT){		//Links
 			a[0] = -1;
 			a[1] = 0;
 		}
 		
-		if(Key.getKeyCode() == Key.VK_RIGHT){
+		if(Key.getKeyCode() == Key.VK_RIGHT){		//Rechts
 			a[0] = 1;
 			a[1] = 0;
 		}
 		
-		if(Key.getKeyCode() == Key.VK_DOWN){
+		if(Key.getKeyCode() == Key.VK_DOWN){		//Unten
 			a[0] = 0;
 			a[1] = -1;
+		}
+		if(Key.getKeyCode() == Key.VK_SPACE){		//Bombe
+			
 		}
 		//b = KeyListenerTest.zeichnen(b, a);			//An dieser Stelle muss die Print-Funktion mit Übergabe des a[] gesetzt werden.
 	}											//Zudem muss der KeyListener für die Bombe-Taste noch eine Auswirkung bekommen.
