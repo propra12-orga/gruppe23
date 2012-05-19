ffa6fbeaf8e7c33c1d764a5cbe2d9ee853a9b63e
Kolja Salewski <kolja.salewski@t-online.de>
2012-05-20 00:21:44 +0200

    Menue & Map in gemeinsames Fenster gepackt

--- src/Map.java
+++ src/Map.java
@@ -1,106 +1,82 @@
-import java.awt.EventQueue;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
 
 import javax.swing.ImageIcon;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JLabel;
 import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
 
-public class Map extends JFrame {
-
-	private JPanel contentPane;
+public class Map extends JPanel {
+	private static final long serialVersionUID = 1L;
 	public boolean gefunden;
 	public GameKeyListener kListener;
-	public int n = 11;// Spielfeldgrï¿½ï¿½e
+	public int n = 11;// Spielfeldgröße
 	public int positionX, positionY;
 	public int[] neuePosition;
-	
-	public int map [][];
-	
-	/*
-	 * Das Spielfeld muss vom Typ Panel sein!!!
-	 * D.h.: neue Klasse und ummodeln.
-	 * 
-	 * 
-	 * 
-	 * 
-	 */
+	int[][] map = {	{4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4},
+			{4, 1, 2, 2, 2, 2, 2, 2, 2, 2, 4},
+			{4, 2, 4, 2, 4, 2, 4, 2, 4, 2, 4},
+			{4, 2, 2, 2, 2, 2, 2, 2, 2, 2, 4},
+			{4, 2, 4, 2, 4, 2, 4, 2, 4, 2, 4},
+			{4, 2, 2, 2, 2, 2, 2, 2, 2, 2, 4},
+			{4, 2, 4, 2, 4, 2, 4, 2, 4, 2, 4},
+			{4, 2, 2, 2, 2, 2, 2, 2, 2, 2, 4},
+			{4, 2, 4, 2, 4, 2, 4, 2, 4, 2, 4},
+			{4, 2, 2, 2, 2, 2, 2, 2, 2, 2, 4},
+			{4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4} };
 
-	public Map(int gameMap[][]) {
-		n = 11;						// Spielfeldgroesse
-		
+	public Map() {
 		gefunden = false;
 		
 		// int[][] map = new int[n][n];
 
 		// for (int a = 0; a < n; a++) {
-		// for (int b = 0; b < n; b++) { // map initialisieren
+		// for (int b = 0; b < n; b++) { // Map initialisieren
 		// map[a][b] = 4;
 		// }
-		// } fï¿½r map normaler weise die maploaderklasse verwenden.
-//		int[][] map = { {4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, },
-//			{ 4, 1, 2, 2, 2, 2, 2, 2, 2, 2, 4, },
-//			{ 4, 2, 4, 2, 4, 2, 4, 2, 4, 2, 4, },
-//			{ 4, 2, 2, 2, 2, 2, 2, 2, 2, 2, 4, },
-//			{ 4, 2, 4, 2, 4, 2, 4, 2, 4, 2, 4, },
-//			{ 4, 2, 2, 2, 2, 2, 2, 2, 2, 2, 4, },
-//			{ 4, 2, 4, 2, 4, 2, 4, 2, 4, 2, 4, },
-//			{ 4, 2, 2, 2, 2, 2, 2, 2, 2, 2, 4, },
-//			{ 4, 2, 4, 2, 4, 2, 4, 2, 4, 2, 4, },
-//			{ 4, 2, 2, 2, 2, 2, 2, 2, 2, 2, 4, },
-//			{ 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, }, };
-		
-		
-		
+		// } für Map normaler Weise die Maploaderklasse verwenden.		
+	}
+	
+	public void zeichnen() {
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < n; j++) {
-
 				if (map[i][j] == 1) {
 					gefunden = true;
-					positionX = i; 	// methode sucht hulk im map - array
-					positionY = j;	// postion x und y sind dann die postionen vom
-									// hulk, fï¿½r keylistener wichtig!
+					positionX = i; 	// Methode sucht Hulk im Map-Array
+					positionY = j;	// Postion x und y sind dann die postionen vom
+									// Hulk, für Keylistener wichtig!
 					break;
-				} else {
-					gefunden = false;
-					
+				} 
+				
+				else {
+					gefunden = false;				
+				}
 				
 			}
-		}
+			
+		}		
 		
-
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 550, 580); // Panel erstellen
 		
-		
-		setBounds(100, 100, 550, 580);// panel erstellen
-		contentPane = new JPanel();
-		
-		contentPane.setBorder(new EmptyBorder(3, 3, 3, 3));
-		setContentPane(contentPane);
+		setBorder(new EmptyBorder(3, 3, 3, 3));
 		GridBagLayout gbl_contentPane = new GridBagLayout();
 		gbl_contentPane.columnWidths = new int[] { 1, 1, 1, 1, 0 };
 		gbl_contentPane.rowHeights = new int[] { 1, 0, 0 };
 		gbl_contentPane.columnWeights = new double[] { 1.0, 1.0, 1.0, 1.0,
 				Double.MIN_VALUE };
 		gbl_contentPane.rowWeights = new double[] { 0.0, 0.0, Double.MIN_VALUE };
-		contentPane.setLayout(gbl_contentPane);
-		setResizable(false);
+		setLayout(gbl_contentPane);
 
-		JLabel[][] label = new JLabel[n][n];// ERstellt ein JLabel array,
+		JLabel[][] label = new JLabel[n][n]; // erstellt ein JLabel-Array
 		GridBagConstraints[][] gbc_label = new GridBagConstraints[n][n];
-		for (i = 0; i < n; i++) {
+		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < n; j++) {
-				label[i][j] = new JLabel("");// fï¿½r jedes einzele arrayelement
-												// wird ein neues label erstellt
+				label[i][j] = new JLabel(""); // für jedes einzele Arrayelement wird ein neues Label erstellt
 
 				label[i][j].setHorizontalAlignment(SwingConstants.CENTER);
-				malen(label[i][j], map[i][j]);// lï¿½dt in das label das
-												// bild(map
-												// hat die werte ob hulk,etc..
+				label_laden(label[i][j], map[i][j]); // lädt in das Label das Bild (Map hat die Werte ob Hulk, etc..)
 				gbc_label[i][j] = new GridBagConstraints();
 				gbc_label[i][j].anchor = GridBagConstraints.CENTER;
 				gbc_label[i][j].fill = GridBagConstraints.NONE;
@@ -109,63 +85,45 @@ public class Map extends JFrame {
 				gbc_label[i][j].insets = new Insets(0, 0, 0, 0);
 				gbc_label[i][j].gridx = i;
 				gbc_label[i][j].gridy = j;
-				contentPane.add(label[i][j], gbc_label[i][j]);// label auf panel
-																// laden
+				add(label[i][j], gbc_label[i][j]); // Label auf Panel laden
 			}
-		}}
-		//pack();
+			
+		}
+		
 	}
-
 	
-	public void malen(JLabel label, int i) {
+	public void label_laden(JLabel label, int i) {
 
 		switch (i) {
-		case 1:// Hulk
-			label.setIcon(new ImageIcon(Map.class
-					.getResource("/Pics/Hulk.png")));
-			break;
-
-		case 2:// Weg
-			label.setIcon(new ImageIcon(Map.class.getResource("/Pics/Weg.png")));
-			break;
-
-		case 3:// Block
-			label.setIcon(new ImageIcon(Map.class
-					.getResource("/Pics/Block.png")));
-			break;
-
-		case 4:// Mauer nicht zerstï¿½rbar(eventuell ï¿½ber hulkmodus)
-			label.setIcon(new ImageIcon(Map.class
-					.getResource("/Pics/Mauer.png")));
-			break;
-		case 5:// bombe
-			label.setIcon(new ImageIcon(Map.class
-					.getResource("/Pics/Bombe.png")));
-			break;
-		case 6:// explosion
-			label.setIcon(new ImageIcon(Map.class.getResource("/Pics/EXP.png")));
-			break;
-		case 7:
-			label.setIcon(new ImageIcon(Map.class.getResource("/Pics/Ausgang.png")));
-			break;
+			case 1: // Hulk
+				label.setIcon(new ImageIcon(Map.class.getResource("/Pics/Hulk.png")));
+				break;
+	
+			case 2: // Weg
+				label.setIcon(new ImageIcon(Map.class.getResource("/Pics/Weg.png")));
+				break;
+	
+			case 3: // Block
+				label.setIcon(new ImageIcon(Map.class.getResource("/Pics/Block.png")));
+				break;
+	
+			case 4: // Mauer nicht zerstörbar (eventuell über Hulkmodus)
+				label.setIcon(new ImageIcon(Map.class.getResource("/Pics/Mauer.png")));
+				break;
+				
+			case 5: // Bombe
+				label.setIcon(new ImageIcon(Map.class.getResource("/Pics/Bombe.png")));
+				break;
+				
+			case 6: // Explosion
+				label.setIcon(new ImageIcon(Map.class.getResource("/Pics/EXP.png")));
+				break;
+				
+			case 7: // Ausgang
+				label.setIcon(new ImageIcon(Map.class.getResource("/Pics/Ausgang.png")));
+				break;
 		}
-	}
-	
-
 		
-	
-//	public static void main(String[] args) {
-//	EventQueue.invokeLater(new Runnable() {
-//	public void run() {
-//		try {
-//			Map frame = new Map();
-//				frame.setVisible(true);
-//			}
-//		catch (Exception e) {
-//				e.printStackTrace();
-//			}
-//		}
-//		});
-//	}
+	}		
 
 }
--- src/Menue.java
+++ src/Menue.java
@@ -1,84 +1,44 @@
-import java.awt.BorderLayout;
-import java.awt.Component;
 import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-
 import javax.swing.AbstractAction;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
-import javax.swing.JPanel;
-import javax.swing.JPopupMenu;
-import javax.swing.border.EmptyBorder;
 
+// Panel für KeyListener benötigt!
 
-//Panel fÃ¼r KeyListener benÃ¶tigt!
-
-
-public class Menue{
+public class Menue {
 
 	private JFrame frame;
 	private final Action_Beenden Action_Beenden = new Action_Beenden();
 	private final Action_Neu Action_Neu = new Action_Neu();
 	private MenueKeyListener MenueListener;
 	private GameKeyListener GameListener;
+	private Map Game = new Map();
+
+	// private Map Player = new Hulk(2,2);														//<<<<----- 
 	
-	public int gameMap[][] = { {4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, },
-			{ 4, 1, 2, 2, 2, 2, 2, 2, 2, 2, 4, },
-			{ 4, 2, 4, 2, 4, 2, 4, 2, 4, 2, 4, },
-			{ 4, 2, 2, 2, 2, 2, 2, 2, 2, 2, 4, },
-			{ 4, 2, 4, 2, 4, 2, 4, 2, 4, 2, 4, },
-			{ 4, 2, 2, 2, 2, 2, 2, 2, 2, 2, 4, },
-			{ 4, 2, 4, 2, 4, 2, 4, 2, 4, 2, 4, },
-			{ 4, 2, 2, 2, 2, 2, 2, 2, 2, 2, 4, },
-			{ 4, 2, 4, 2, 4, 2, 4, 2, 4, 2, 4, },
-			{ 4, 2, 2, 2, 2, 2, 2, 2, 2, 2, 4, },
-			{ 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, }, };
-	
-	//private Map Player = new Hulk(2,2);
-	
-	
-	//private Map Game = new Map(gameMap);															//<<<<----- 
-	
-	//
-	//Es muss noch ein Frame hinzugefÃ¼gt werden, in dem das Spielfeld ist:
-	//Wichtig ist hier, dass .addKeyListener(GameKeyListener) zu diesem Frame hinzugefÃ¼gt wird!
-	//
-	//
+	// Es muss noch ein Frame hinzugefügt werden, in dem das Spielfeld ist:
+	// Wichtig ist hier, dass .addKeyListener(GameKeyListener) zu diesem Frame hinzugefügt wird!
 	
 	public Menue() {
-		initialize();
-		
+		initialize();		
 	}
-
-	/**
-	 * Initialize the contents of the frame.
-	 */
 	
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 475, 309);
+		frame.add(Game);
+		frame.setBounds(100, 100, 570, 616);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		JMenuBar menuBar = new JMenuBar();
 		frame.setJMenuBar(menuBar);
 		
-		JMenu mnSpiel = new JMenu("Spiel");//	public static void main(String[] args) {
-//		EventQueue.invokeLater(new Runnable() {
-//		public void run() {
-//			try {
-//				Map frame = new Map();
-//					frame.setVisible(true);
-//				}
-//			catch (Exception e) {
-//					e.printStackTrace();
-//				}
-//			}
-//			});
-//		}
+		JMenu mnSpiel = new JMenu("Spiel");
+		
+		Game.zeichnen();
+		
 		menuBar.add(mnSpiel);
 		menuBar.addKeyListener(MenueListener);		//KeyListener wird fuer's Menue implementiert
 		menuBar.isFocusable();
@@ -89,62 +49,33 @@ public class Menue{
 		
 		JMenuItem mntmBeenden = new JMenuItem("Beenden");
 		mntmBeenden.setAction(Action_Beenden);
-		mnSpiel.add(mntmBeenden);
-		
-		
-	
-		
-		/*JPanel panel = new JPanel();
-		frame.getContentPane().add(panel, BorderLayout.CENTER);
-		panel.setLayout(null);
-		*/
-		
+		mnSpiel.add(mntmBeenden);		
 	}
 
-	private static void addPopup(Component component, final JPopupMenu popup) {
-		component.addMouseListener(new MouseAdapter() {
-			public void mousePressed(MouseEvent e) {
-				if (e.isPopupTrigger()) {
-					showMenu(e);
-				}
-			}
-			public void mouseReleased(MouseEvent e) {
-				if (e.isPopupTrigger()) {
-					showMenu(e);
-				}
-			}
-			private void showMenu(MouseEvent e) {
-				popup.show(e.getComponent(), e.getX(), e.getY());
-			}
-		});
-		
-	}
-	
 	private class Action_Beenden extends AbstractAction {
-		/*Beenden-Funktion wird implementiert 
-		 */
-
+		private static final long serialVersionUID = 1L;
 		public Action_Beenden() {
 			putValue(NAME, "Beenden");
 			putValue(SHORT_DESCRIPTION, "Beenden des Spiels");
 		}
+		
 		public void actionPerformed(ActionEvent e) {
 			System.exit( 0 );
 		}
+		
 	}
 	
-	/*
-	 * Neustarten-Funktion wird implementiert
-	 */
-	
 	private class Action_Neu extends AbstractAction {
+		private static final long serialVersionUID = 1L;
 		public Action_Neu() {
 			putValue(NAME, "Neu");
 			putValue(SHORT_DESCRIPTION, "Neustart des Spiels");
 		}
+		
 		public void actionPerformed(ActionEvent e) {
 			initialize();
 		}
+		
 	}
 	
 	public static void main(String[] args) {
@@ -153,7 +84,9 @@ public class Menue{
 				try {
 					Menue window = new Menue();
 					window.frame.setVisible(true);
-				} catch (Exception e) {
+				} 
+				
+				catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
