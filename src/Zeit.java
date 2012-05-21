512b408ec6bf7f6665d2ff66ef4449e6c50ab73f
Andrej <andrej.morlang@hhu.de>
2012-05-21 17:14:16 +0200

    Bombe hat einen festen radius und terminiert

--- src/Zeit.java
+++ src/Zeit.java
@@ -3,21 +3,25 @@ import java.util.TimerTask;
 
 import javax.swing.JLabel;
 
-public class Zeit extends JLabel
-{
+public class Zeit extends JLabel {
 	private static final long serialVersionUID = 1L;
 	Timer timer;
-	
+
 	// timer_starten-Methode
-	public void timer_starten(int x, String logo) { // x = Timer-Verögerung, logo = Bomben-/Detonations-Icon
+	public void timer_starten(int x, String logo) { // x = Timer-Verögerung,
+													// logo =
+													// Bomben-/Detonations-Icon
 		timer = new Timer(); // Timer erstellen
-		
+
 		if (logo.equals("Bombe")) {
-			timer.schedule(new bombe(), x); // Zeit bis zur Detonation um x Millisek. verzögern
+			timer.schedule(new bombe(), x); // Zeit bis zur Detonation um x
+											// Millisek. verzögern
 		}
-		
+
 		else if (logo.equals("Detonation")) {
-			timer.schedule(new detonation(), x); // Zeit bis zum Ende der Detonation um x Millisek. verzögern
+			timer.schedule(new detonation(), x); // Zeit bis zum Ende der
+													// Detonation um x Millisek.
+													// verzögern
 		}
 
 	}
@@ -26,22 +30,37 @@ public class Zeit extends JLabel
 		public void run() {
 			System.out.println("Bombe explodiert"); // Test
 			System.out.println("");
-			
+
 			Menue.get_game().bombe_detonieren(); // Detonation der Bombe
-			timer.cancel(); // Timer terminieren				
+			timer.cancel(); // Timer terminieren
 		}
-		
+
 	}
-	
+
 	public class detonation extends TimerTask {
 		public void run() {
 			System.out.println("Detonation beendet"); // Test
 			System.out.println("");
-			
+
 			timer.cancel(); // Timer terminieren
-			Menue.map[Menue.get_game().bomb.get_x()][Menue.get_game().bomb.get_y()] = 2; // Nach Ablauf des Timers wieder das Weg-Icon darstellen
+			for (int x = -1, y = -1; x < 2; x++, y++) {
+				if (Menue.map[Menue.get_game().bomb.get_x() + x][Menue
+						.get_game().bomb.get_y()] == 6)
+					Menue.map[Menue.get_game().bomb.get_x() + x][Menue
+							.get_game().bomb.get_y()] = 2; // Nach Ablauf des
+															// Timers wieder das
+															// Weg-Icon
+															// darstellen
+				if (Menue.map[Menue.get_game().bomb.get_x()][Menue.get_game().bomb
+						.get_y() + y] == 6)
+					Menue.map[Menue.get_game().bomb.get_x()][Menue.get_game().bomb
+							.get_y() + y] = 2;
+			}
+
+			Menue.get_game().removeAll();
+			Menue.get_game().refresh();
 		}
-		
+
 	}
 
 }
