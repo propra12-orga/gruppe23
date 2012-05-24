bb9d369b16c968515ecb24faacb4f361eea32890
Marvin Buecker <Marvin.Buecker@uni-duesseldorf.de>
2012-05-24 12:19:53 +0200

    Groesse der map angepasst

--- src/Menue.java
+++ src/Menue.java
@@ -27,8 +27,8 @@ public class Menue implements KeyListener {
 
 	private static Map game = new Map(map); // Spielfeld erstellen
 	private static Hulk hulk = new Hulk(); // Hulk erstellen
-	//private Thread moveHulk;
-	//private Thread bombHulk;
+	// private Thread moveHulk;
+	// private Thread bombHulk;
 	private static boolean move;
 
 	private static boolean bomb;
@@ -40,11 +40,12 @@ public class Menue implements KeyListener {
 		initialize();
 		a = new int[2];
 
-		//	moveHulk = new Thread(new MoveHulk());			//Threads initialisieren und starten
-		//	moveHulk.start();
+		// moveHulk = new Thread(new MoveHulk()); //Threads initialisieren und
+		// starten
+		// moveHulk.start();
 
-		//	bombHulk = new Thread(new BombHulk());
-		//	bombHulk.start();
+		// bombHulk = new Thread(new BombHulk());
+		// bombHulk.start();
 
 		bomb = false;
 		move = false;
@@ -52,41 +53,50 @@ public class Menue implements KeyListener {
 
 	// Methode zum Initialisieren des Spielfelds:
 	private void initialize() {
-		frame = new JFrame(); 									// Fenster erstellen
-		frame.setTitle("Bomberhulk");							// Fenstertitel setzen
-		frame.setBounds(100, 100, 550, 610); 					// Fenstergroesse einstellen (x-Position, y-Position, Breite, Hoehe)
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 	// Programm beim Schliessen des Fensters beenden
-		frame.setResizable(false); 								// Fenster soll nicht skalierbar sein
+		frame = new JFrame(); // Fenster erstellen
+		frame.setTitle("Bomberhulk"); // Fenstertitel setzen
+		frame.setBounds(100, 100, 550, 605); // Fenstergroesse einstellen
+												// (x-Position, y-Position,
+												// Breite, Hoehe)
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Programm beim
+																// Schliessen
+																// des Fensters
+																// beenden
+		frame.setResizable(false); // Fenster soll nicht skalierbar sein
 
-		JMenuBar menuBar = new JMenuBar(); 	// Menueleiste erstellen
-		frame.setJMenuBar(menuBar); 		// Menueleiste hinzufuegen
+		JMenuBar menuBar = new JMenuBar(); // Menueleiste erstellen
+		frame.setJMenuBar(menuBar); // Menueleiste hinzufuegen
 
 		menuBar.isFocusable();
 
 		JMenu mnSpiel = new JMenu("Spiel"); // Menuepunkt "Spiel" erstellen
-		menuBar.add(mnSpiel); 				// Menuepunkt "Spiel" hinzufuegen
+		menuBar.add(mnSpiel); // Menuepunkt "Spiel" hinzufuegen
 
-		JMenuItem mntmNeu = new JMenuItem("Neu"); 	// Untermenuepunkt "Neu" erstellen
-		mnSpiel.add(mntmNeu); 						// Untermenuepunkt "Neu" hinzufuegen
-		mntmNeu.setAction(Action_Neu); 				// Aktion "Action_Neu" hinzufuegen
+		JMenuItem mntmNeu = new JMenuItem("Neu"); // Untermenuepunkt "Neu"
+													// erstellen
+		mnSpiel.add(mntmNeu); // Untermenuepunkt "Neu" hinzufuegen
+		mntmNeu.setAction(Action_Neu); // Aktion "Action_Neu" hinzufuegen
 
-		JMenuItem mntmBeenden = new JMenuItem("Beenden"); 	// Untermenuepunkt "Beenden" erstellen
-		mnSpiel.add(mntmBeenden); 							// Untermenuepunkt "Beenden" hinzufuegen
-		mntmBeenden.setAction(Action_Beenden); 				// Aktion "Action_Beenden" hinzufuegen
+		JMenuItem mntmBeenden = new JMenuItem("Beenden"); // Untermenuepunkt
+															// "Beenden"
+															// erstellen
+		mnSpiel.add(mntmBeenden); // Untermenuepunkt "Beenden" hinzufuegen
+		mntmBeenden.setAction(Action_Beenden); // Aktion "Action_Beenden"
+												// hinzufuegen
 
-		frame.add(game); 	// Spielfeld hinzufuegen
-		game.init(); 		// Spielfeld zeichnen
+		frame.add(game); // Spielfeld hinzufuegen
+		game.init(); // Spielfeld zeichnen
 
-		game.addKeyListener(this); 	// Keylistener zum Spielfeld hinzufuegen
-		game.setFocusable(true);	// Spielfeld fokussierbar machen
-		game.requestFocus();		// Fokus auf Spielfeld setzen
+		game.addKeyListener(this); // Keylistener zum Spielfeld hinzufuegen
+		game.setFocusable(true); // Spielfeld fokussierbar machen
+		game.requestFocus(); // Fokus auf Spielfeld setzen
 	}
 
 	// keyPressed-Methode:
 	public void keyPressed(KeyEvent Key) {
 		// oben:
 		if (Key.getKeyCode() == KeyEvent.VK_UP) {
-			System.out.println("oben"); 			// Test
+			System.out.println("oben"); // Test
 			System.out.println();
 
 			a[0] = 0;
@@ -97,7 +107,7 @@ public class Menue implements KeyListener {
 
 		// Pfeiltaste links:
 		if (Key.getKeyCode() == KeyEvent.VK_LEFT) {
-			System.out.println("links"); 			// Test
+			System.out.println("links"); // Test
 			System.out.println();
 
 			a[0] = -1;
@@ -108,7 +118,7 @@ public class Menue implements KeyListener {
 
 		// Pfeiltaste rechts:
 		if (Key.getKeyCode() == KeyEvent.VK_RIGHT) {
-			System.out.println("rechts"); 			// Test
+			System.out.println("rechts"); // Test
 			System.out.println();
 
 			a[0] = 1;
@@ -119,7 +129,7 @@ public class Menue implements KeyListener {
 
 		// Pfeiltaste unten:
 		if (Key.getKeyCode() == KeyEvent.VK_DOWN) {
-			System.out.println("unten"); 			// Test
+			System.out.println("unten"); // Test
 			System.out.println();
 
 			a[0] = 0;
@@ -130,20 +140,27 @@ public class Menue implements KeyListener {
 
 		// Leertaste:
 		if (Key.getKeyCode() == KeyEvent.VK_SPACE) {
-			System.out.println("Space"); 			// Test
+			System.out.println("Space"); // Test
 			System.out.println();
 
-			if (Map.get_bomb().get_x() == 0 && Map.get_bomb().get_y() == 0) { // falls nicht schon eine andere Bombe liegt
+			if (Map.get_bomb().get_x() == 0 && Map.get_bomb().get_y() == 0) { // falls
+																				// nicht
+																				// schon
+																				// eine
+																				// andere
+																				// Bombe
+																				// liegt
 				bomb = true;
 
 				game.bombe_legen();
-				game.removeAll(); 	// entferne alle bisherigen Komponenten vom Panel
-				game.refresh(); 	// zeichne alle Komponenten des Panels neu
+				game.removeAll(); // entferne alle bisherigen Komponenten vom
+									// Panel
+				game.refresh(); // zeichne alle Komponenten des Panels neu
 			}
 		}
 
 		if (Key.getKeyCode() != KeyEvent.VK_SPACE) {
-			if (Map.map[hulk.get_x() + a[0]][hulk.get_y() + a[1]] == 2) { 	// falls
+			if (Map.map[hulk.get_x() + a[0]][hulk.get_y() + a[1]] == 2) { // falls
 																			// das
 																			// naechste
 																			// Feld
@@ -152,8 +169,9 @@ public class Menue implements KeyListener {
 																			// ist
 
 				game.move_Hulk(a[0], a[1]); // bewege Hulk auf dem Spielfeld
-				game.removeAll(); 			// entferne alle bisherigen Komponenten vom Panel
-				game.refresh(); 			// zeichne alle Komponenten des Panels neu
+				game.removeAll(); // entferne alle bisherigen Komponenten vom
+									// Panel
+				game.refresh(); // zeichne alle Komponenten des Panels neu
 			}
 
 			else if (Map.map[hulk.get_x() + a[0]][hulk.get_y() + a[1]] == 7) { // falls
@@ -163,10 +181,10 @@ public class Menue implements KeyListener {
 																				// das
 																				// Ziel-Feld
 																				// ist
-				System.out.println("Gewonnen!"); 			// Test
+				System.out.println("Gewonnen!"); // Test
 				System.out.println();
 
-				System.out.println("Spiel neugestartet"); 	// Test
+				System.out.println("Spiel neugestartet"); // Test
 				System.out.println();
 
 				// Hulk zurueckpositionieren:
@@ -181,17 +199,17 @@ public class Menue implements KeyListener {
 				game.refresh();
 			}
 
-			else if (Map.map[hulk.get_x() + a[0]][hulk.get_y() + a[1]] == 6) { 	// falls
+			else if (Map.map[hulk.get_x() + a[0]][hulk.get_y() + a[1]] == 6) { // falls
 																				// das
 																				// naechste
 																				// Feld
 																				// ein
 																				// Explosions-Feld
 																				// ist
-				System.out.println("Verloren!"); 			// Test
+				System.out.println("Verloren!"); // Test
 				System.out.println();
 
-				System.out.println("Spiel neugestartet"); 	// Test
+				System.out.println("Spiel neugestartet"); // Test
 				System.out.println();
 
 				// Hulk zurueckpositionieren:
